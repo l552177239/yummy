a063daa8d873fe0140be80ab50b62000e49f2628
l552177239 <552177239@qq.com>
2017-07-25 09:58:01 +0000

    add logo

--- src/ui/pages/Home/Home.js
+++ src/ui/pages/Home/Home.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from './logo.svg'
 
 class Home extends Component{
   render(){
     return(
-      <div className="Home">
-        Home
+      <div className="home">
+        <div className='hero'>
+        {/* hero最醒目的版块 */}
+        <img className='app-logo' src={logo} alt='logo' />
+        <h1 className='title'>
+          吮指
+        </h1>
+        <p className='slogan'>
+          享受舌尖艳遇
+        </p>
+        <div className='actions'>
+          <Link to='/signup'>注册</Link>
+          <Link to='/login'>登录</Link>
+        </div>
+        </div>
       </div>
     )
   }
