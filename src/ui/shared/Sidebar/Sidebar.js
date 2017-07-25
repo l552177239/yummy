196053a5b5340fa67852265525e80c2946bba4f4
l552177239 <552177239@qq.com>
2017-07-25 10:53:54 +0000

    react-burger-menu

--- src/ui/shared/Sidebar/Sidebar.js
+++ src/ui/shared/Sidebar/Sidebar.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { slide as Menu } from 'react-burger-menu'
+import './sidebar.css'
 
 class Sidebar extends Component{
   render(){
     return(
       <div className="Sidebar">
-        <Link to='/'>首页</Link>
-        <Link to='/signup'>注册</Link>
+        <Menu>
+          <div className='bm-user-info'>
+            UserInfo
+          </div>
+          <div className='bm-link-list'>
+            <Link to='/'>首页</Link>
+            <Link to='/signup'>注册</Link>
+          </div>
+          <button className='bm-close-button'>关闭</button>
+        </Menu>
       </div>
     )
   }
