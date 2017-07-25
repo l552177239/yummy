2f3b8ea91b75d7a0d6dbcd2543fa5379dfdc9c72
l552177239 <552177239@qq.com>
2017-07-25 21:43:13 +0000

    sidebar css 修改

--- src/ui/shared/Sidebar/Sidebar.js
+++ src/ui/shared/Sidebar/Sidebar.js
@@ -3,23 +3,39 @@ import { Link } from 'react-router-dom'
 import { slide as Menu } from 'react-burger-menu'
 import './sidebar.css'
 
-class Sidebar extends Component{
-  render(){
+class Sidebar extends Component {
+  state = {
+    isOpen: false
+  }
+  closeBmMenu = () => {
+    this.setState({
+      isOpen: false
+    })
+  }
+  render() {
     return(
-      <div className="Sidebar">
-        <Menu isOpen>
-          <div className='bm-user-info'>
-            <img src='http://media.haoduoshipin.com/yummy/default-avatar.png' alt='avata' />
-            <div className='bm-user-info'>
-              <Link className='bm-user-left'>Liu</Link>
-              <Link className='bm-user-right'>退出</Link>
+      <div className="sidebar">
+        <Menu isOpen={this.state.isOpen}>
+          <div className="bm-user-info">
+            <img src="http://media.haoduoshipin.com/yummy/default-avatar.png" alt="avatar" />
+            <div className="bm-user-auth">
+              <Link to="" className="bm-user-left">
+                happypeter
+              </Link>
+              <Link to="" className="bm-user-right">
+                退出
+              </Link>
             </div>
           </div>
-          <div className='bm-link-list'>
-            <Link to='/'>首页</Link>
-            <Link to='/signup'>注册</Link>
+          <div className="bm-link-list">
+            <Link onClick={this.closeBmMenu} to="/">Home</Link>
+            <Link onClick={this.closeBmMenu} to="/signup">注册</Link>
+            <Link onClick={this.closeBmMenu} to="/cart">购物车</Link>
+            <Link onClick={this.closeBmMenu} to="/dishes">猜你喜欢</Link>
           </div>
-          <button className='bm-close-button'>关闭</button>
+          <button onClick={this.closeBmMenu} className="bm-close-button">
+            关闭
+          </button>
         </Menu>
       </div>
     )
@@ -27,6 +43,3 @@ class Sidebar extends Component{
 }
 
 export default Sidebar
-
-
-//筛选公司
