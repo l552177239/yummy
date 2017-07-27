e3377d5bcd58850dd059c39239cb8a46747a5b79
l552177239 <552177239@qq.com>
2017-07-27 15:25:15 +0000

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
+
 import './App.css'
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom'
+import store from './redux/store'
+
+import { Provider } from 'react-redux'
 
 import Home from './ui/pages/Home/Home'
 import Signup from './ui/pages/Signup/Signup'
@@ -13,25 +17,28 @@ import Sidebar from './ui/shared/Sidebar/Sidebar'
 import AlertBox from './ui/shared/AlertBox/AlertBox'
 import Dashboard from './ui/pages/Dashboard/Dashboard'
 
-class App extends Component{
+class App extends React.Component{
   render(){
     return(
-      <div className="App">
-        <Router>
-          <div>
-            <AlertBox />
-            <Route render={({ location }) => {
-                  return location.pathname !== '/' ?
-                  (<Sidebar />) : null
-                }} />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
-          </Switch>
+      <Provider store={store}>
+        <div className="App">
+          <Router>
+            <div>
+              <AlertBox />
+              <Route render={({ location }) => {
+                    return location.pathname !== '/' ?
+                    (<Sidebar />) : null
+                  }} />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/login' component={Login} />
+              <Route path='/signup' component={Signup} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+          </Router>
         </div>
-        </Router>
-      </div>
+      </Provider>
     )
   }
 }
---
+++ src/redux/reducers/accountReducer.js
@@ -0,0 +1,15 @@
+let defaultAccountState = {
+  isAuthenticated: false,
+  currentUser: ''
+}
+
+export default function accountReducer(state=defaultAccountState, action) {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return {...state, isAuthenticated: true, currentUser: action.username}
+    case 'LOG_OUT':
+      return state;
+    default:
+      return state
+  }
+}
